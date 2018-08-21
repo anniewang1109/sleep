1e9ca213493bd064d53e06ac38a9472b2bc1c708
Annie Wang <anwang911@gmail.com>
2018-08-20 22:33:09 -0400

    fixed both quiz

--- myths.js
+++ myths.js
@@ -4,14 +4,21 @@ var mythsArr = [
 "Myth 3: People need less sleep as they get older",
 "Myth 4: Extra sleep for one night can cure you of problems with excessive daytime fatigue",
 "Myth 5: You can make up for lost sleep during the week by sleeping more on the weekends",
-"Myth 6: Naps aren't a bad thing"];
-var answerArr = ["false", "false", "false", "false","false", "true"]
-var incorrectAnswerArr = ["true", "true", "true", "true", "true", "false"]
+"Myth 6: Naps aren't a bad thing",
+"Myth 7: Getting just 1 hour less sleep per night than needed will not have any effect on your daytime functioning."];
+var answerArr = ["false", "false", "false", "false","false", "true", "false"]
+var incorrectAnswerArr = ["true", "true", "true", "true", "true", "false", "true"]
 var answerInfoArr = ["No evidence shows that any major organ (including the brain) or regulatory system in the body shuts down during sleep. Some physiological processes actually become more active while you sleep. For example, secretion of certain hormones is boosted, and activity of the pathways in the brain linked to learning and memory increases. ",
 "Your biological clock makes you most alert during the daytime and least alert at night. Thus, even if you work the night shift, you will naturally feel sleepy when nighttime comes. Most people can reset their biological clock, but only by appropriately timed cues—and even then, by 1–2 hours per day at best. Consequently, it can take more than a week to adjust to a substantial change in your sleep–wake cycle—for example, when traveling across several time zones or switching from working the day shift to the night shift.",
 "Older people don’t need less sleep, but they may get less sleep or find their sleep less refreshing. That’s because as people age, the quality of their sleep changes. Older people are also more likely to have insomnia or other medical conditions that disrupt their sleep.",
 "Not only is the quantity of sleep important, but also the quality of sleep. Some people sleep 8 or 9 hours a night but don’t feel well rested when they wake up because the quality of their sleep is poor. A number of sleep disorders and other medical conditions affect the quality of sleep. Sleeping more won’t lessen the daytime sleepiness these disorders or conditions cause. However, many of these disorders or conditions can be treated effectively with changes in behavior or with medical therapies. Additionally, one night of increased sleep may not correct multiple nights of inadequate sleep.",
-"Although naps are no substitute for a good night’s sleep, they can be restorative and help counter some of the effects of not getting enough sleep at night. Naps can actually help you learn how to do certain tasks quicker. But avoid taking naps later than 3 p.m., particularly if you have trouble falling asleep at night, as late naps can make it harder for you to fall asleep when you go to bed. Also, limit your naps to no longer than 20 minutes, because longer naps will make it harder to wake up and get back in the swing of things. If you take more than one or two planned or unplanned naps during the day, you may have a sleep disorder that should be treated."]
+"Although this sleeping pattern will help you feel more rested, it will not completely make up for the lack of sleep or correct your sleep debt. This pattern also will not necessarily make up for impaired performance during the week or the physical problems that can result from not sleeping enough. Furthermore, sleeping later on the weekends can affect your biological clock, making it much harder to go to sleep at the right time on Sunday nights and get up early on Monday mornings.",
+"Although naps are no substitute for a good night’s sleep, they can be restorative and help counter some of the effects of not getting enough sleep at night. Naps can actually help you learn how to do certain tasks quicker. But avoid taking naps later than 3 p.m., particularly if you have trouble falling asleep at night, as late naps can make it harder for you to fall asleep when you go to bed. Also, limit your naps to no longer than 20 minutes, because longer naps will make it harder to wake up and get back in the swing of things. If you take more than one or two planned or unplanned naps during the day, you may have a sleep disorder that should be treated.",
+"This lack of sleep may not make you noticeably sleepy during the day. But even slightly less sleep can affect your ability to think properly and respond quickly, and it can impair your cardiovascular health and energy balance as well as your body’s ability to fight infections, particularly if lack of sleep continues. If you consistently do not get enough sleep, a sleep debt builds up that you can never repay. This sleep debt affects your health and quality of life and makes you feel tired during the day."]
+
+console.log(mythsArr.length)
+console.log(answerInfoArr.length)
+
 
 var question = document.querySelector("#question");
 var t = document.querySelector("[value*='true']");
@@ -51,10 +58,10 @@ function submit(qNum){
   //show the answer and answer info
   if (qValue == answerArr[qNum]){
     answer.innerHTML = "You are correct! It is " + answerArr[qNum] +  ".";
-    answerInfo.innerHTML = answerInfoArr[qNum]
+    answerInfo.innerHTML = "<br>" + answerInfoArr[qNum] + "<br><br><i>Source: NIH</i>"
   }else if (qValue == incorrectAnswerArr[qNum]){
     answer.innerHTML = "You are wrong! It is " + answerArr[qNum] + ".";
-    answerInfo.innerHTML = answerInfoArr[qNum]
+    answerInfo.innerHTML = "<br>" + answerInfoArr[qNum] + "<br><br><i>Source: NIH</i>"
   }else{
     answer.innerHTML = "Select an answer!"
   }
--- yourSleep.js
+++ yourSleep.js
@@ -7,13 +7,21 @@ var allQuestions = [
     },
     {
         question: "On average, how many hours of sleep do you get every night?",
-        choices: ["Less than 5", "5-6", "7-8", "9-10", "11-12", "13-14", "15-16", "17-19", "More than 19"]
+        choices: ["14 to 17 hours", "12 to 15 hours", "11 to 14 hours", "10 to 13 hours","9 to 11 hours","8 to 10 hours","7 to 9 hours","7 to 8 hours", "Less than 7 hours"]
     },
     {
         question: "Select all that applies: what do you do in your bed or before you go to bed? ",
-        choices: ["Watch TV", "Talk to another person", "Eat", "Sleep with the lights on","Intake caffeine"]
+        choices: ["Use electronic device(s)", "Talk to another person", "Eat", "Sleep with the lights on","Intake caffeine"]
     },
 ]
+function write(){
+    for (i=0;i<allQuestions[0].choices.length;i++){
+        console.log(allQuestions[0].choices[i] + " = " + allQuestions[0].response[i])
+    }
+}
+
+write();
+
 
 //qArr.forEach(function(value, index, array))
 function build(){
@@ -27,6 +35,7 @@ function build(){
                 console.log(answerArr)
             }else{
                 answerArr.push(`<input class="choice" type= "radio" name="q${questionNum}" value = "${choice}"> ${currentQ.choices[choice]}<br>`)
+                console.log(answerArr)
             }
         }
         console.log(questionNum)
@@ -44,19 +53,20 @@ function build(){
 }
 build()
 
-var q0 = document.querySelector("[name*='q0']").value;
-var q1 = document.querySelector("[name*='q1']").value;
-var q2 = document.querySelector("[name*='q2']").value;
 var result = []
 
 function q0Result(){
+    var q0 = document.quiz.q0.value;
+
     console.log(q0)
+    // console.log(document.querySelector("input[name='q0']"))
     allQuestions[0].choices.forEach(function(currentChoice, choiceNum){
         //if question value equals choice number then append message to array
         // console.log(choiceNum)
         if (q0 == choiceNum){
             // console.log(choiceNum + "!")
             var q0Choice = allQuestions[0].response[choiceNum]
+            // console.log(q0Choice)
             var q0Message= `<p>The recommended hours of sleep you should get is ${q0Choice}.</p>`
             result.push(q0Message)
             // console.log(choiceNum +"before return")
@@ -68,21 +78,28 @@ function q0Result(){
 }
 
 function q1Result(){
-    if (q1 == q0Result) {
+    var q1 = document.quiz.q1.value;
+    console.log(q1)
+    if (q1 == 6 || 7 && q0Result == 6){
         var q1Message = "<p>You've been sleeping the right amount! Good job!</p>"
-    } else{
+    }
+    else if (q1 == q0Result) {
+        var q1Message = "<p>You've been sleeping the right amount! Good job!</p>"
+    }
+    else{
         var q1Message = "<p>You're sleeping the wrong number of hours!</p>"
     };
     result.unshift(q1Message)
 }
 
 function q2Result(){
-    var q2Message = `<p>To sleep well avoid:<br>- watching TV<br>- sleeping with lights on<br>- intaking caffeine before you sleep<br>- minimize noise and light in your room</p> `
+    var q2 = document.quiz.q2.value;
+    var q2Message = `<p>To sleep well avoid:<br>- using electronic device(s)<br>- sleeping with lights on<br>- intaking caffeine before you sleep<br>- minimize noise and light in your room</p> `
     result.push(q2Message)
 }
 
 function showResults(){
-    console.log(result)
+    // console.log(result)
     var resultMsg = document.createElement("div")
     resultMsg.setAttribute("id","resultMessage")
     document.querySelector(".content").append(resultMsg);
@@ -90,5 +107,5 @@ function showResults(){
     q1Result();
     q2Result();
     resultMsg.innerHTML = result.join("")
-    console.log(result)
+    // console.log(result)
 }
